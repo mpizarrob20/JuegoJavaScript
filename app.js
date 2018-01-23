c8fc732a1ce1b39c86bf16df392e6bf33c0b7d00
mpizarrob20 <mpizarrob20@gmail.com>
2018-01-23 16:06:05 -0300

    update js

--- app.js
+++ app.js
@@ -13,52 +13,60 @@ En cada turno, un jugador tira un dado tantas veces como desee. Cada resultado s
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
-    //1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    if(gamePlaying){
+         //1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
     
-    //2. Display result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        //2. Display result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
     
     
-    //3. Update the round score IF the rolled number was NOT a 1
-    if(dice !== 1){
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        nextPlayer();
+        //3. Update the round score IF the rolled number was NOT a 1
+        if(dice !== 1){
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
        
+        }        
     }
+    
+   
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    //Add CURRENT score to GLOBAL score
-    scores[activePlayer] += roundScore;
+    if(gamePlaying){
+        //Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
     
-    //Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        //Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
-    //Check if player won the game
-    if(scores[activePlayer] >= 20){
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-        
+        //Check if player won the game
+        if(scores[activePlayer] >= 20){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
     } else {
         //Next player
         nextPlayer();
     }
+        
+    }
+    
     
         
 });
@@ -90,6 +98,7 @@ function init(){
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
 
     document.querySelector('.dice').style.display = 'none';
 
